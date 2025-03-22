c370dbcc2d15bf38a95b2a45140676436d57597e
BDNRZ <czarekbednarski@gmail.com>
2025-03-22 16:34:13 +0100

    Kamien nie kamieniuje, bąble działają

--- PytongĆw3Zad1.py
+++ PytongĆw3Zad1.py
@@ -12,29 +12,32 @@ def bubble_sort_asc(lista):
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
                 if not swapped:
                     break
+    print("iteracje rosnąco: " + str({iteration_count}))
+    return lista
 
-def bubble_sort_desc(lista):
-    iteration_count = 0
-    n = len(lista)
+def bubble_sort_desc(lista_desc):
+    iteration_count_desc = 0
+    n = len(lista_desc)
     for i in range(n):
-        iteration_count += 1
+        iteration_count_desc += 1
         swapped = False
         for j in range(0, n - i - 1):
-            if lista[j] < lista[j + 1]:
+            if lista_desc[j] < lista_desc[j + 1]:
                 swapped = True
-                lista[j + 1], lista[j] = lista[j], lista[j + 1]
+                lista_desc[j + 1], lista_desc[j] = lista_desc[j], lista_desc[j + 1]
                 if not swapped:
                     break
+    print("iteracje malejąco: " + str({iteration_count_desc}))
+    return lista_desc
     
-    print({iteration_count})
-    return lista
+   
 
 sorted_asc = bubble_sort_asc(NIU.copy())
 sorted_desc = bubble_sort_desc(NIU.copy())
 
 print("Lista przed sortowaniem:")
 print(NIU)
-print("lista po sortowaniu:")
+print("lista po sortowaniu malejąco:")
 print(sorted_asc)
-print("print desc:")
+print("lista po sortowaniu rosnąco:")
 print(sorted_desc)
--- PytongĆw3Zad2.py
+++ PytongĆw3Zad2.py
@@ -7,7 +7,9 @@ while True:
     def game_random(list):
         won = False
         draw = False
-        player_choice = input("Wybierz K dla Kamień, P dla Papier, lub N dla Nożyce")
+        player_choice = input("Wybierz K dla Kamień, P dla Papier, lub N dla Nożyce: ")
+        if player_choice != choices:
+            print("Niewłaściwy znak")
         if player_choice == "K" and randomised == "N":
             won = True
             draw = False
@@ -38,20 +40,21 @@ while True:
         if won == True and draw == False:
             player_score =+ 1
             print("wygrana")
-            print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
-        if won == False and draw == True:
+            #print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
+        elif won == False and draw == True:
             print("remis")
-            print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
-        if won == False and draw == False:
+            #print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
+        elif won == False and draw == False:
             computer_score =+ 1
             print("przegrana")
-            print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
+            #print("wynik gracza: " + str(player_score) + "wynik komutera: " + str(computer_score))
 
     game_random(choices)
     print(game_random)
+    print("Wynik: " + str(player_score) + " do: " + str(computer_score))
     print("grać dalej?")
-    if input("T dla tak, N dla nie") == "T":
+    if input("T dla tak, N dla nie: ") == "T":
         game_random(choices)
         print(game_random)
-    if input("T dla tak, N dla nie") == "N":
-        print("OK")
+        if input("T dla tak, N dla nie: ") == "N":
+            print("OK")
