c9fcfcd202562488065b12008bb79ba324da5354
BDNRZ <czarekbednarski@gmail.com>
2025-03-26 19:35:52 +0100

    Przerobiony main z projektu

--- main_test.py
+++ main_test.py
@@ -8,6 +8,8 @@ def serial_number_giver(lenght=12):
 
 df = pd.read_csv('test_list_copy.csv')
 
+initial_rows = len(df)
+
 if 'Serial Number' not in df.columns:
     df['Serial Number'] = None
 
@@ -24,13 +26,22 @@ def get_serial_number(x):
 
 while True:
     new_ship = input("Podaj nazwę nowego statku/statków (Zostaw puste aby zapisać dodane statki)").strip()
-    new_row = {"Name": new_ship}
+    if new_ship == "":
+        break
+    amount = input("Podaj ilość nowych statków:")
+    new_row = {"Name": new_ship, "Serial Number": "", "Amount": amount}
     df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-    amount = input("Podaj ilość nowych droidów:")
-    new_row = {"Amount": amount}
+    
 
-df['Serial Number'] = df['Serial Number'].apply(
-    lambda x: x if pd.notna(x) and str(x).strip() != '' else serial_number_giver())
+df['Serial Number'] = df.apply(
+    lambda row: row['Serial Number'] if pd.notna(row['Serial Number']) and str(row['Serial Number']).strip() != ''
+                else (serial_number_giver() if pd.notna(row['Name']) and str(row['Name']).strip() != '' else None),
+    axis=1
+)
+df['Amount'] = df['Amount'].apply(
+    lambda x: x if pd.notna(x) and str(x).strip() != '' else amount)
 
 df.to_csv('test_list_copy.csv', index=False)
-
+added_rows = df.iloc[initial_rows:]
+print("Dodano następujące statki:")
+print(added_rows[['Name', 'Serial Number', 'Amount']].to_string(index=False))
